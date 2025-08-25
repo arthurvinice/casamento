8890b0201409d02f8d931ef1ca557e8db7d89324
arthurvinice <arthurvinice@gmail.com>
2025-08-25 17:40:02 -0300

    bg do footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -210,7 +210,7 @@
     </main>
 
     <!-- Footer: Start -->
-    <footer class="landing-footer bg-body footer-text">
+    <footer class="landing-footer bg-primary footer-text">
         <div class="footer-bottom py-3 py-md-5">
             <div
                 class="container d-flex flex-wrap justify-content-between flex-md-row flex-column text-center text-md-start">
